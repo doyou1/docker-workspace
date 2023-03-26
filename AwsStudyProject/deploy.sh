19db23421aaddeed52190d4fcc83e9cdad7f03a1
doyou1 <tla149@daum.net>
2023-03-26 21:26:45 +0900

    deploy.sh update

--- AwsStudyProject/deploy.sh
+++ AwsStudyProject/deploy.sh
@@ -2,13 +2,13 @@
 sudo docker ps -a -q --filter "name=awsstudy" | grep -q . && docker stop awsstudy && docker rm awsstudy | true
 
 # delete exist image
-sudo docker rmi jh/awsstudy:1.0
+sudo docker rmi tla149/awsstudy:1.0
 
 # pull image from docker hub
-sudo docker pull jh/awsstudy:1.0
+sudo docker pull tla149/awsstudy:1.0
 
 # run docker
-docker run -d -p 8080:8080 --name awsstudy jh/awsstudy:1.0
+docker run -d -p 8080:8080 --name awsstudy tla149/awsstudy:1.0
 
 # Delete unused and unnecessary images -> Images currently being bitten by containers will not be deleted.
 docker rmi -f $(docker images -f "dangling=true" -q) || true
